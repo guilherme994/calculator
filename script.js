99b46954f73f12810b5d230acc6c457a2862b800
Guilherme Barbosa <85406858+guilherme994@users.noreply.github.com>
2021-07-01 23:35:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var result = document.getElementById("result");
+
+function insert(num) {
+    let numero = result.innerHTML;
+    if (numero == "ERROR!") {
+        clean()
+        result.innerHTML = num; 
+    } else {
+        result.innerHTML = numero + num;    
+    }  
+}
+
+function clean() {
+    result.innerHTML = "";
+}
+
+function calc() {
+    let result = document.getElementById("result").innerHTML;
+    if (result) {
+        document.getElementById("result").innerHTML = eval(result);
+    } else {
+        document.getElementById("result").innerHTML = "ERROR!";
+    }
+}
+
+function back() {
+    result.innerHTML = result.innerHTML.substring(0, result.innerHTML.length -1);
+}
